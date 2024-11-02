dcb81dabf0a50680ea31e8738246d48bd309b979
BEOM0191 <fkdlej6343@naver.com>
2024-11-02 18:09:28 +0900

    BlinkandMap
    공용스킬인 블링크와 맵수정 및 로비맵 생성하였습니다

--- Source/P_Cube/CubeGameplayTags.h
+++ Source/P_Cube/CubeGameplayTags.h
@@ -168,6 +168,7 @@ public:
 	FGameplayTag Abilities_Assassin_Attack;
 	FGameplayTag Abilities_Assassin_Basic;
 	FGameplayTag Abilities_Assassin_Unique;
+	FGameplayTag Abilities_Scroll_Blink;
 
 	FGameplayTag Cooldown_FIreStaff_Attack;
 	FGameplayTag Cooldown_FIreStaff_Basic;
@@ -211,6 +212,7 @@ public:
 	FGameplayTag Cooldown_Assassin_Attack;
 	FGameplayTag Cooldown_Assassin_Basic;
 	FGameplayTag Cooldown_Assassin_Unique;
+	FGameplayTag Cooldown_Scroll_Blink;
 
 	FGameplayTag CombatSocket_ActorLocation;
 	FGameplayTag CombatSocket_Weapon;
