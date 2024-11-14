749e9c4049d8c0173baca69eef0b8080d98fdf66
groun519 <groun519@gmail.com>
2024-11-14 18:27:22 +0900

    Add CC feacher
    군중제어, 상태이상 기능

--- Source/P_Cube/Actor/CubeProjectile.cpp
+++ Source/P_Cube/Actor/CubeProjectile.cpp
@@ -85,6 +85,45 @@ void ACubeProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, 
 	{
 		if (UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(OtherActor))
 		{
+			/** Knockback **/
+			const bool bKnockback = DamageEffectParams.Knockback_ForceMagnitude != 0.f;
+			if ( bKnockback )
+			{
+				FRotator Rotation = GetActorRotation();
+				Rotation.Pitch = 0.f;
+
+				const FVector KnockbackDirection = Rotation.Vector();
+				const FVector KnockbackForce = KnockbackDirection * DamageEffectParams.Knockback_ForceMagnitude;
+				DamageEffectParams.Knockback_Force = KnockbackForce;
+			}
+			/** end Knockback **/
+
+			/** Grab **/
+			const bool bGrab = DamageEffectParams.Grab_ForceMagnitude != 0.f;
+			if ( bGrab )
+			{
+				FRotator Rotation = GetActorRotation();
+				Rotation.Pitch = 0.f;
+
+				const FVector GrabDirection = Rotation.Vector();
+				const FVector GrabForce = GrabDirection * DamageEffectParams.Grab_ForceMagnitude;
+				DamageEffectParams.Grab_Force = GrabForce;
+			}
+			/** end Grab **/
+
+			/** Airborne **/
+			const bool bAirborne = DamageEffectParams.Airborne_ForceMagnitude != 0.f;
+			if ( bAirborne )
+			{
+				FRotator Rotation = GetActorRotation();
+				Rotation.Pitch = 90.f;
+
+				const FVector AirborneDirection = Rotation.Vector();
+				const FVector AirborneForce = AirborneDirection * DamageEffectParams.Airborne_ForceMagnitude;
+				DamageEffectParams.Airborne_Force = AirborneForce;
+			}
+			/** end Airborne **/
+
 			DamageEffectParams.TargetAbilitySystemComponent = TargetASC;
 			UCubeAbilitySystemLibrary::ApplyDamageEffect(DamageEffectParams); // damage 이펙트 적용.
 		}
